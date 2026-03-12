e5c29d2106ee7299c908c6c6eb7ed19576d8f99c
andreas <Andreas.Zwander@gmail.com>
2026-03-13 00:21:20 +0100

    Add system_info module and logic for init system detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod system_info;
+
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let system_info = system_info::SystemInfo::new();
+    println!("Der Name von Prozess ID 1 ist: '{:?}'", system_info.init);
+
+    match system_info.init {
+        system_info::InitType::Systemd => println!("Logik: Ich erstelle .mount Dateien."),
+        system_info::InitType::OpenRc => println!("Logik: Ich nutze die /etc/fstab."),
+        system_info::InitType::Unknown(init_name) => println!("Logik: Unbekannter Init-Prozess: {}", init_name),
+    }
 }
